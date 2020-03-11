aa5fb0cd23a589f8830767f487bac4b3fa88ff0f
MSchueler <zenloki91@hotmail.com>
2020-03-11 18:19:04 -0400

    Add project files.

---
+++ Controllers/FileHelperController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileHelperController : ControllerBase
+    {
+        private List<string> myfiles = new List<string>();
+
+        [HttpGet]
+        [Route("getshitdone")]
+        public ActionResult<List<string>> Get()
+        {
+            myfiles = GetFiles();
+            return Ok(myfiles);
+        }
+
+        private List<string> GetFiles()
+        {
+            var docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var myDir = Directory.GetDirectories(docPath);
+            try
+            {
+
+                // cboNames.Items.Add(docPath);
+
+                foreach (var item in myDir)
+                {
+                    if (item != null)
+                    {
+                        myfiles.Add(item);
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+
+            return myfiles;
+        }
+    }
+}
---
+++ Models/FileHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApi.Models
+{
+    public class FileHelper
+    {
+        public string myfiles { get; set; }
+    }
+}
